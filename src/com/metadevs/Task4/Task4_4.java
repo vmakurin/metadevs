155eef990f9a72b0ca9c3a01271bf0fcb39faa5a
vmakurin <vmakurin@meta-devs.io>
2022-02-15 21:22:32 +0300

    Add Task 4_4.

--- src/com/metadevs/Task4/Task4_4.java
+++ src/com/metadevs/Task4/Task4_4.java
@@ -1,4 +1,18 @@
 package com.metadevs.Task4;
 
 public class Task4_4 {
+    public static void main(String[] args) {
+        int[][] array = new int[3][5];
+        array = new int[][]{{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {11, 12, 13, 14, 15}};
+        int[][] invArray = new int[3][5];
+        for (int row = 0; row < array.length; row++) {
+            System.out.println();
+            for (int col = 0; col < array[row].length; col++) {
+                int rowIndex = array.length - 1 - row;
+                int colIndex = array[row].length - 1 - col;
+                invArray[row][col] = array[rowIndex][colIndex];
+                System.out.print (invArray[row][col] + " ");
+            }
+        }
+    }
 }
