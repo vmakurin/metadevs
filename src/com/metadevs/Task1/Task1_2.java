dbc6968b7a9f8d64c2609d95aca6852b30dc99fb
vmakurin <vmakurin@meta-devs.io>
2022-02-21 21:05:36 +0300

    Add line indent \Task1_3 Add naming && add input of variables sout \Task1_3

--- src/com/metadevs/Task1/Task1_2.java
+++ src/com/metadevs/Task1/Task1_2.java
@@ -5,10 +5,13 @@ import java.util.Scanner;
 public class Task1_2 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+        System.out.println("Введите value1: ");
         int value1 = s.nextInt();
+        System.out.println("Введите value2: ");
         int value2 = s.nextInt();
+
         System.out.println("Сумма: " + (value1 + value2));
-        System.out.println("Разность" + (value1 - value2));
+        System.out.println("Разность: " + (value1 - value2));
         System.out.println("Произведение: " + (value1 * value2));
         System.out.println("Частное: " + (value1 / value2));
     }
--- src/com/metadevs/Task1/Task1_3.java
+++ src/com/metadevs/Task1/Task1_3.java
@@ -5,13 +5,16 @@ import java.util.Scanner;
 public class Task1_3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String ln = sc.nextLine();
-        String n = sc.nextLine();
-        int a = sc.nextInt();
+        System.out.println("Введите фамилию: ");
+        String lastName = sc.nextLine();
+        System.out.println("Введите имя: ");
+        String name = sc.nextLine();
+        System.out.println("Введите возраст: ");
+        int age = sc.nextInt();
 
-        System.out.println("Фамилия: "+ ln);
-        System.out.println("Имя: "+ n);
-        System.out.println("Возраст: "+ a);
+        System.out.println("Фамилия: "+ lastName);
+        System.out.println("Имя: "+ name);
+        System.out.println("Возраст: "+ age);
 
     }
 }
