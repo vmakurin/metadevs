4a416587f8ee8b67211de52210a667a29b9a6b57
vmakurin <vmakurin@meta-devs.io>
2022-02-23 16:01:16 +0300

    Fix code review.

--- src/com/metadevs/Task4/Task4_1.java
+++ src/com/metadevs/Task4/Task4_1.java
@@ -4,8 +4,21 @@ import java.util.Scanner;
 
 public class Task4_1 {
     public static void main(String[] args) {
-        Scanner n = new Scanner(System.in);
-        int k = n.nextInt();
+        getFractal();
+    }
+
+    public static int scanInt(Scanner scan) {
+        while (!scan.hasNextInt()) {
+            System.out.println("Это не число");
+            scan.next();
+        }
+        return scan.nextInt();
+    }
+
+    public static void getFractal() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите размер фрактала:");
+        int k = scanInt(scan);
         for (int i = 0, f = 1; i < k; i++) {
             f += f * i;
             System.out.println(f);
--- src/com/metadevs/Task4/Task4_2.java
+++ src/com/metadevs/Task4/Task4_2.java
@@ -6,14 +6,22 @@ public class Task4_2 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Введите число value1");
-        int value1 = scan.nextInt();
+        int value1 = scanInt(scan);
         System.out.println("Введите число value2");
-        int value2 = scan.nextInt();
+        int value2 = scanInt(scan);
         printEven(value1, value2);
     }
 
+    public static int scanInt(Scanner scan) {
+        while (!scan.hasNextInt()) {
+            System.out.println("Это не число");
+            scan.next();
+        }
+        return scan.nextInt();
+    }
+
     public static void printEven(int x, int y) {
-        for (int i = x; i < y; i++) {
+        for (int i = x; i <= y; i++) {
             if (i % 2 == 0) {
                 System.out.println(i);
             }
